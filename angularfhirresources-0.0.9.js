c5a92a469c98b53c5b49f1ca426ae090e89dcff2
safrisk <sarakristinafrisk@gmail.com>
2015-12-21 16:49:53 +0100

    Build

--- angularfhirresources-0.0.9.js
+++ angularfhirresources-0.0.9.js
@@ -749,6 +749,69 @@ angular.module('angularFhirResources')
 
 /**
  * @ngdoc service
+ * @name angularFhirResources.fhirMedication
+ * @description
+ * # fhirMedication
+ * Factory in the angularFhirResources.
+ */
+angular.module('angularFhirResources')
+  .factory('fhirMedication', ['$http', 'fhirConfig', 'Utilities', function ($http, fhirConfig, Utilities) {
+    // Service logic
+    var baseUrl = fhirConfig.url;
+    var resourceType = 'MedicationOrder';
+    var resourcePrefix = resourceType + '/';
+
+    // Public API here
+    return {
+      /**
+       * Get all registered Medication
+       * @returns {*}
+       */
+      getMedicationOrdersForPatient: function (patient) {
+        var url = baseUrl + resourceType;
+        return $http({
+          method: 'GET',
+          url: url,
+          headers: fhirConfig.headers,
+          params: {
+            patient: patient
+          }
+        }).then(function (response) {
+          return Utilities.getFhirResourceList(response.data.entry);
+        });
+      },
+      createMedicationOrder: function (medicationOrder) {
+        medicationOrder.resourceType = resourceType;
+        var url = baseUrl + resourceType;
+        return $http({
+          method: 'POST',
+          url: url,
+          headers: fhirConfig.headers,
+          data: medicationOrder
+        });
+      },  
+      /**
+       * Empty MedicationOrder template
+       * @returns { patient: {}, prescriber: {}, medicationReference: {}, dateWritten: {}, dosageInstruction: [{ text: {} }] }
+       */
+      initiateEmptyMedicationOrder: function () {
+        return {
+          patient: {},
+          prescriber: {},
+          medicationReference: {},
+          dateWritten: {},
+          dosageInstruction: [{ 
+            text: {}
+          }]
+        };
+      }
+    };
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc service
  * @name angularFhirResources.fhirObservation
  * @description
  * # fhirObservation
