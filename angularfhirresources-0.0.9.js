290d9d7c4fa2dd5024ba0e8e4380c15bf82ef594
safrisk <sarakristinafrisk@gmail.com>
2016-01-13 15:31:10 +0100

    Build

--- angularfhirresources-0.0.9.js
+++ angularfhirresources-0.0.9.js
@@ -893,15 +893,15 @@ angular.module('angularFhirResources')
           }
         );
       },
-      getObservationsByPatientId: function (patientId, dateRange, code, sampleRate) {
+      getObservationsByPatientId: function (patientId, dateRange, code, samplingPeriod) {
 				var requestParams = {
             subject: patientId,
             date: dateRange,
             code: code,
             _format: 'json'
           };
-				if(sampleRate) {
-					requestParams.sampleRate = sampleRate;
+				if(samplingPeriod) {
+					requestParams.samplingPeriod = samplingPeriod;
 				}
         return $http({
           method: 'GET',
@@ -912,7 +912,7 @@ angular.module('angularFhirResources')
           return response.data;
         });
       },
-      getObservationsByDeviceId: function (deviceId, dateRange, code, sampleRate) {
+      getObservationsByDeviceId: function (deviceId, dateRange, code, samplingPeriod) {
         if(deviceId.indexOf('Device/') === 0){
           deviceId = deviceId.substr('Device/'.length);
         }
@@ -922,8 +922,8 @@ angular.module('angularFhirResources')
             code: code,
             _format: 'json'
           };
-				if(sampleRate) {
-					requestParams.sampleRate = sampleRate;
+				if(samplingPeriod) {
+					requestParams.samplingPeriod = samplingPeriod;
 				}
         return $http({
           method: 'GET',
