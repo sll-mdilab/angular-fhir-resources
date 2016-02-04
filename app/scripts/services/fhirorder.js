1ca3580654033909f12723ca9a1bb96d044130f7
safrisk <sarakristinafrisk@gmail.com>
2016-02-04 12:52:39 +0100

    v0.2.6

---
+++ app/scripts/services/fhirorder.js
@@ -0,0 +1,90 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularFhirResources.fhirOrder
+ * @description
+ * # fhirOrder
+ * Factory in the angularFhirResources.
+ */
+angular.module('angularFhirResources')
+  .factory('fhirOrder', ['$http', 'fhirConfig', 'Utilities', function ($http, fhirConfig, Utilities) {
+    // Service logic
+    var baseUrl = fhirConfig.url;
+    var resourceType = 'Order';
+    var resourcePrefix = resourceType + '/';
+
+    // Public API here
+    return {
+      /**
+       * Get all registered Order 
+       * @returns {*}
+       */
+      getOrdersForPatient: function (patient) {
+        var url = baseUrl + resourceType;
+        return $http({
+          method: 'GET',
+          url: url,
+          headers: fhirConfig.headers,
+          params: {
+            patient: patient,
+          }
+        }).then(function (response) {
+          return Utilities.getFhirResourceList(response);
+        });
+      },
+      /**
+       * Create new Order.
+       * @param order The complete Order object.
+       * @returns {*}
+       */
+      createOrder: function (order) {
+        order.resourceType = resourceType;
+        var url = baseUrl + resourceType;
+        return $http({
+          method: 'POST',
+          url: url,
+          headers: fhirConfig.headers,
+          data: order
+        });
+      },
+      /**
+       * Update existing Order object.
+       * @param order The complete updated Order object. order.id determines what object will be replaced.
+       * @returns {*}
+       */
+      updateOrder: function (order) {
+        order.resourceType = resourceType;
+        var url = baseUrl + resourceType + '/' + order.id;
+        return $http({
+          method: 'PUT',
+          url: url,
+          headers: fhirConfig.headers,
+          data: order
+        });
+      },  
+      /**
+       * Empty Order template
+       * @returns { date: {}, subject: { reference: {} }, source: { reference: {} }, detail : [{ reference: {} }],
+       *            extension: [{ url: "http://sll-mdilab.net/fhir/Order#alarmlimits", extension: [{ url: upper, valueDecimal: {} }, { url: lower, valueDecimal: {} }] }] }
+       */
+      initiateEmptyOrder: function () {
+        return {
+          date: {},
+          subject: { reference: {} },
+          source: { reference: {} },
+          detail : [{ reference: {} }],
+          extension: [{
+            url: "http://sll-mdilab.net/fhir/Order#alarmlimits",
+            extension: [{
+              url: upper,
+              valueDecimal: {}
+            }, {
+              url: lower,
+              valueDecimal: {}
+            }]
+          }]
+        };
+      }
+    };
+  }]);
