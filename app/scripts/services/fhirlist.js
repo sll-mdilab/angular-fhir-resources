bc65bb892b82397b6ce754ae6ede09d5cde0fd79
safrisk <sarakristinafrisk@gmail.com>
2016-02-12 11:46:03 +0100

    v0.2.7

---
+++ app/scripts/services/fhirlist.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularFhirResources.fhirList
+ * @description
+ * # fhirList
+ * Factory in the angularFhirResources.
+ */
+angular.module('angularFhirResources')
+  .factory('fhirList', ['$http', 'fhirConfig', 'Utilities', function ($http, fhirConfig, Utilities) {
+    // Service logic
+    var baseUrl = fhirConfig.url;
+    var resourceType = 'List';
+    var resourcePrefix = resourceType + '/';
+
+    // Public API here
+    return {
+      /**
+       * Get a List by params
+       * @param params A param object
+       * {
+       *  listId: 'ID of a List',
+       *  includeResourceType: 'optional to include resource type as prefix in request'
+       * }
+       * @returns {*}
+       */
+      getList: function () {
+        var url = baseUrl;
+        if (params.includeResourceType) {
+          url += resourcePrefix;
+        }
+        url += params.listId;
+        return $http({
+          method: 'GET',
+          url: url,
+          headers: fhirConfig.headers
+        }).then(function (response) {
+          return response.data;
+        });
+      },
+      /**
+       * Empty List template
+       * @returns { code: { text: {} }
+       */
+      initiateEmptyList: function () {
+        return {
+          status: "current",
+          mode: "working",
+          entry: []
+        };
+      }
+    };
+  }]);
