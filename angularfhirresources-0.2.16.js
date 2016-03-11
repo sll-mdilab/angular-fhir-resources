7e1833fad97c328070d661ec06ef7489bafc90f3
safrisk <sarakristinafrisk@gmail.com>
2016-03-11 09:48:18 +0100

    v0.2.16

--- angularfhirresources-0.2.16.js
+++ angularfhirresources-0.2.16.js
@@ -61,32 +61,6 @@ angular.module('angularFhirResources')
 
 /**
  * @ngdoc service
- * @name angularFhirResources.fhirAuthInterceptor
- * @description
- * # fhirAuthInterceptor
- * Service in the angularFhirResources.
- */
-angular.module('angularFhirResources')
-  .factory('fhirAuthInterceptor', ['fhirConfig', '$q', '$location', function (fhirConfig, $q, $location) {
-  
-  return {
-
-   'responseError': function(rejection) {
-      if (rejection.status == 401) {
-        fhirConfig.clearAuthToken();
-        $location.path('oauth');   
-      }
-      return $q.reject(rejection);
-    }
-  }
-
-}]);
-
-
-'use strict';
-
-/**
- * @ngdoc service
  * @name angularFhirResources.fhirAppointment
  * @description
  * # fhirAppointment
@@ -167,6 +141,32 @@ angular.module('angularFhirResources')
       }
     };
   }]);
+
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularFhirResources.fhirAuthInterceptor
+ * @description
+ * # fhirAuthInterceptor
+ * Service in the angularFhirResources.
+ */
+angular.module('angularFhirResources')
+  .factory('fhirAuthInterceptor', ['fhirConfig', '$q', '$location', function (fhirConfig, $q, $location) {
+  
+  return {
+
+   'responseError': function(rejection) {
+      if (rejection.status == 401) {
+        fhirConfig.clearAuthToken();
+        $location.path('oauth');   
+      }
+      return $q.reject(rejection);
+    }
+  }
+
+}]);
+
 
 /**
  * @ngdoc service
