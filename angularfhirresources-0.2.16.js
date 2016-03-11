c330bab40d7906ec9d657d3d91b121e71f564abc
safrisk <sarakristinafrisk@gmail.com>
2016-03-11 10:17:59 +0100

    v0.2.16

--- angularfhirresources-0.2.16.js
+++ angularfhirresources-0.2.16.js
@@ -1831,8 +1831,8 @@ angular.module('angularFhirResources')
     // Public API here
     return {
       /**
-       * Get all patients
-       * @returns {*} a list of Patient objects
+       * Get all questionnare responses for a patient
+       * @returns {*} a list of QuestionnaireResponse objects
        */
       getQuestionnaireResponsesForPatient: function (patient) {
         var url = baseUrl + resourceType;
@@ -1845,6 +1845,21 @@ angular.module('angularFhirResources')
           }
         }).then(function (response) {
           return Utilities.formatFhirResponse(response);
+        });
+      },
+      /**
+       * Update existing QuestionnaireResponse object.
+       * @param questionnaireResponse The complete updated QuestionnaireResponse object. questionnaireResponse.id determines what object will be replaced.
+       * @returns {*}
+       */
+      updateQuestionnaireResponse: function (questionnaireResponse) {
+        questionnaireResponse.resourceType = resourceType;
+        var url = baseUrl + resourceType + '/' + questionnaireResponse.id;
+        return $http({
+          method: 'PUT',
+          url: url,
+          headers: fhirConfig.headers,
+          data: questionnaireResponse
         });
       }
     };
--- app/scripts/services/fhirquestionnaireresponse.js
+++ app/scripts/services/fhirquestionnaireresponse.js
@@ -17,8 +17,8 @@ angular.module('angularFhirResources')
     // Public API here
     return {
       /**
-       * Get all patients
-       * @returns {*} a list of Patient objects
+       * Get all questionnare responses for a patient
+       * @returns {*} a list of QuestionnaireResponse objects
        */
       getQuestionnaireResponsesForPatient: function (patient) {
         var url = baseUrl + resourceType;
@@ -31,6 +31,21 @@ angular.module('angularFhirResources')
           }
         }).then(function (response) {
           return Utilities.formatFhirResponse(response);
+        });
+      },
+      /**
+       * Update existing QuestionnaireResponse object.
+       * @param questionnaireResponse The complete updated QuestionnaireResponse object. questionnaireResponse.id determines what object will be replaced.
+       * @returns {*}
+       */
+      updateQuestionnaireResponse: function (questionnaireResponse) {
+        questionnaireResponse.resourceType = resourceType;
+        var url = baseUrl + resourceType + '/' + questionnaireResponse.id;
+        return $http({
+          method: 'PUT',
+          url: url,
+          headers: fhirConfig.headers,
+          data: questionnaireResponse
         });
       }
     };
